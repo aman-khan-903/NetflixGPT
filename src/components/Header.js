e65d7235ccb060780a963792e4ea2313b752c333
aman-khan-903 <amankhanvds22@gmail.com>
2024-05-02 17:32:36 +0530

    Build SignIn And SignUp Form

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='absolute p-4 bg-gradient-to-b from-black'>
+    <div className='absolute p-4 bg-gradient-to-b from-black z-10'>
         <img className='w-48' src='https://images.ctfassets.net/y2ske730sjqp/821Wg4N9hJD8vs5FBcCGg/9eaf66123397cc61be14e40174123c40/Vector__3_.svg?w=460' alt='logo' />
     </div>
   )
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm]= useState(true); 
+
+  const toggleSignInForm= ()=>{
+    setIsSignInForm(!isSignInForm); 
+  }
+
+
   return (
-    <div>
+    <div >
         <Header/>
-        <div>
+        <div className='absolute'>
             <img src='https://assets.nflxext.com/ffe/siteui/vlv3/c7f07b68-7989-4ff7-a31e-11c17dcc2fea/fcf685b8-3f9f-42d8-9af3-4bb86fa5a3b8/IN-en-20240422-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt='bg_Image' />
         </div>
+        <form className='p-4 m-4 absolute top-[145px] left-[550px] bg-black w-3/12 flex flex-col opacity-80 rounded-xl text-white gap-5 '>
+            <h1 className='font-bold text-4xl'>{isSignInForm ? "Sign In": "Sign Up"}</h1>
+
+            {
+              !isSignInForm && <>
+              <input 
+            type='text' 
+            placeholder='Full Name' 
+            className='p-4 mx-auto w-11/12 bg-gray-700 rounded-lg'
+            />
+            <input 
+            type='number' 
+            placeholder='Mobile Number' 
+            className='p-4  mx-auto w-11/12 bg-gray-700 rounded-lg'
+            />
+              </>
+            }
+
+            <input 
+            type='text' 
+            placeholder='Email Address' 
+            className='p-4 mx-auto w-11/12 bg-gray-700 rounded-lg'
+            />
+
+            <input 
+            type='password' 
+            placeholder='Password' 
+            className='p-4  mx-auto w-11/12 bg-gray-700 rounded-lg'
+            />
+
+            <button 
+            className='p-4 my-4  bg-red-700 py-[10px] w-11/12 mx-auto rounded-lg'>
+            {
+              isSignInForm ? "Sign In" : "Sign Up"
+            }
+            </button>
+
+            <span className='p-4' onClick={toggleSignInForm}>
+              {
+                isSignInForm ? 
+                "New to Netflix ? Sign Up Now" : 
+                "Already Registered ? Sign In Now"
+              }
+            </span>
+        </form>
     </div>
   )
 }
